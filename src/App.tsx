f8095c1a86456cdf361294eb6f7cf0cc4873adbb
Aung Ko Ko Minn <maungkokominn@gmail.com>
2026-02-10 22:21:51 +0700

    Initial frontend setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+        <div className="container bg-gray-100">
+            <h1 className="">Hello World</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
